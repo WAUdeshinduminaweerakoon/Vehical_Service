9dc5b1e6519b5924da58e74835151ec5460e8982
WAUdeshinduminaweerakoon <udeshidumina@gmail.com>
2023-11-26 21:09:25 +0530

    cheaking DeleteRegistrationServlet modify

--- src/main/java/com/uniquedeveloper/registration/DeleteRegistrationServlet.java
+++ src/main/java/com/uniquedeveloper/registration/DeleteRegistrationServlet.java
@@ -1,44 +1,91 @@
 package com.uniquedeveloper.registration;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.uniquedeveloper.registration.Registration;
-import com.uniquedeveloper.registration.RegistrationDAO; // Assuming you have a DAO class for handling database operations
 
 @WebServlet("/DeleteRegistrationServlet")
 public class DeleteRegistrationServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+    private static final String JDBC_URL = "jdbc:mysql://172.187.178.153:3306/isec_assessment2";
+    private static final String JDBC_USERNAME = "isec";
+    private static final String JDBC_PASSWORD = "EUHHaYAmtzbv";
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        // Retrieve the registration ID from the form
-        int registrationId = Integer.parseInt(request.getParameter("registrationId"));
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       
+        
 
-        // Assuming you have a DAO class for handling database operations
-        RegistrationDAO registrationDAO = new RegistrationDAO();
+        // Get the bookingid from the request parameter
+        String bookingid = request.getParameter("registrationId");
 
-        try {
-            // Retrieve the registration object by ID
-            Registration registration = registrationDAO.getRegistrationById(registrationId);
+        if (bookingid != null) {
+            // Validate bookingid
+            if (isValidBookingId(bookingid)) {
+                // Delete the reservation
+                boolean deleted = deleteReservation(bookingid);
 
-            if (registration != null) {
-                // Delete the registration
-                registrationDAO.deleteRegistration(registration);
-
-                // Redirect to the page where registrations are listed
-                response.sendRedirect("ListRegistrations.jsp");
+                if (deleted) {
+                    response.sendRedirect(request.getContextPath() + "/booking");
+                } else {
+                  
+                    response.getWriter().println("Failed to delete registration.");
+                }
             } else {
-                // Handle the case where the registration was not found
-                response.getWriter().println("Registration not found!");
+                // Handle the case where bookingid is not a valid integer
+                response.getWriter().println("Invalid bookingid. Please provide a valid bookingid.");
             }
-        } catch (Exception e) {
-            // Handle any exceptions that may occur during the deletion process
-            e.printStackTrace();
-            response.getWriter().println("An error occurred while deleting the registration.");
+        } else {
+            // Handle the case where bookingid is missing in the request
+            response.getWriter().println("Invalid request.");
         }
     }
+
+    // Method to validate bookingid as an integer
+    private boolean isValidBookingId(String bookingid) {
+        try {
+            // Attempt to parse bookingid as an integer
+            Integer.parseInt(bookingid);
+            return true;
+        } catch (NumberFormatException e) {
+            // bookingid is not a valid integer
+            return false;
+        }
+    }
+
+    // Method to delete a reservation from the database
+    private boolean deleteReservation(String bookingid) {
+        try {
+            // Load the JDBC driver and establish a connection to the database
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(JDBC_URL, JDBC_USERNAME, JDBC_PASSWORD);
+
+            // Define a SQL query to delete the reservation
+            String sql = "DELETE FROM vehicle_service WHERE booking_id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, bookingid);
+
+            // Execute the query
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            // Close the resources
+            preparedStatement.close();
+            connection.close();
+
+            // Check if the deletion was successful
+            return rowsAffected > 0;
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            return false; 
+        }
+    }
+
+    
 }
